2171297398bcca55f3575bb26096901d805b7e43
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2017-10-04 15:22:25 -0700

    update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,8 +1,10 @@
-# If bash command fails, build should error out
-set -e
+pip3 install numpy==1.11.0
+pip3 install pandas==0.19.1
+pip3 install scipy==0.18.1
+pip3 install seaborn==0.7.1
 
-##### Install specific package versions with pip #####
-
-# pip3 install pandas==0.19.1
-# pip3 install tweepy==3.5.0
-# pip3 install bs4==4.4.1
+DATADIR=/usr/local/share/datasets
+mkdir -p $DATADIR
+# TODO: Update download links below to the correct url 
+# wget -O $DATADIR/colors.csv https://s3.amazonaws.com/assets.datacamp.com/production/project_2/datasets/colors.csv
+# wget -O $DATADIR/sets.csv https://s3.amazonaws.com/assets.datacamp.com/production/project_2/datasets/sets.csv
